4fbf24599b2d1581b86445c23e4f6e8a46724455
sameer <sam965309@gmail.com>
2024-12-21 11:26:55 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const userRouter = require("./routes/user.js");
 
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
-
+const PORT = process.env.PORT || 8080;
 main()
     .then(() => {
     console.log("connected to DB");
@@ -104,5 +104,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+    console.log(`server listening port ${PORT}`);
 });
